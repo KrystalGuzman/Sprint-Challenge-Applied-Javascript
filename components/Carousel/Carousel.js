9be534ddf7737e4017586fa76c6312a2fc0052e9
Krystal Guzman <kryguzman@gmail.com>
2020-01-17 09:33:01 -0800

    finished cards and started stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,52 @@
     <div class="right-button"> > </div>
   </div>
 */
+const container = document.querySelector('.carousel-container');
+
+function createCarousel(){
+
+	// elements created
+  const carousel = document.createElement('div');
+  const left = document.createElement('div');
+  const mountain = document.createElement('img');
+  const computer = document.createElement('img');
+  const trees = document.createElement('img');
+  const turntable = document.createElement('img');
+  const right = document.createElement('div');
+
+  //classes added
+  carousel.classList.add('carousel');
+  left.classList.add('left-button');
+  mountain.classList.add('carousel1');
+  computer.classList.add('carousel1');
+  trees.classList.add('carousel1');
+  turntable.classList.add('carousel1');
+  right.classList.add('right-button');
+  
+//content added
+  mountain.src = './assets/carousel/mountains.jpeg';
+  computer.src = './assets/carousel/computer.jpeg';
+  trees.src = './assets/carousel/trees.jpeg';
+  turntable.src = './assets/carousel/turntable.jpeg';
+  const rightBtn = '\u25ba';
+  const leftBtn = '\u25c4';
+  
+  // elements structured
+  carousel.append(left);
+  carousel.append(mountain);
+  carousel.append(computer);
+  carousel.append(trees);
+  carousel.append(turntable);
+  carousel.append(right);
+
+  // // add this functionality later
+  // left.addEventListener("click", () => {
+  //   left.classList.toggle("left-button");
+  // });
+  // right.addEventListener("click", () => {
+  //   right.classList.toggle("right-button");
+  // });
+  return carousel;
+}
+console.log(createCarousel());
+
